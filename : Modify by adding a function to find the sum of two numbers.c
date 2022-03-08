2afff7fc5e32eb04ba03b49059a238fbddc5aadc
Jomy Johnson <95912027+jomyjohnson12@users.noreply.github.com>
2022-03-08 20:20:32 +0530

    Create : Modify by adding a function to find the sum of two numbers.c

---
+++ : Modify by adding a function to find the sum of two numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h> int add (int, int); void main ()
+{
+
+int a, b, sum; clrscr ();
+printf ("Enter the numbers"); scanf ("%d%d”, &a,&b); sum=add(a, b);
+printf ("sum=%d”, sum); getch();
+}
+
+int add (int x, int y)
+
+{
+
+return(x+y);
+
+}
